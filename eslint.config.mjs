02b5d4b3e1a4c89158df8c1a4b3b92a230e0ec50
Ronnie DuPree Jr <ronniedupree@gmail.com>
2025-12-11 16:55:29 -0800

    api setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,21 @@
 import js from "@eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 
 export default [
   js.configs.recommended,
+  {
+    ...reactPlugin.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  reactPlugin.configs.flat["jsx-runtime"],
   {
     files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
     languageOptions: {
@@ -15,6 +25,10 @@ export default [
           jsx: true,
         },
       },
+    },
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "react/prop-types": "off",
     },
   },
   prettier,
